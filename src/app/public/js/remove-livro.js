71c349c5b068707bdcbfa69709b065e80a58ec65
Heitor Saldanha <heitorsaldanha@gmail.com>
2020-08-31 21:52:00 +0100

    Repaginando o Layout

--- src/app/public/js/remove-livro.js
+++ src/app/public/js/remove-livro.js
@@ -1,14 +1,15 @@
 let tabelaLivros = document.querySelector('#livros');
-tabelaLivros.addEventListener('click', evento => {
-  let elementoClicado = evento.target;
+tabelaLivros.addEventListener('click', (evento) => {
+    let elementoClicado = evento.target;
 
-  if (elementoClicado.dataset.type === 'remocao') {
-    let livroId = elementoClicado.dataset.ref;
-    fetch(`http://localhost:3000/livros/${livroId}`, { method: 'DELETE'})
-      .then(resp => {
-        let tr = elementoClicado.closest(`#livro_${livroId}`);
-        tr.remove();
-      })
-      .catch(err => console.log('Erro ao deletar o livro'));
-  }
+    if (elementoClicado.dataset.type == 'remocao') {
+        let livroId = elementoClicado.dataset.ref;
+        fetch(`http://localhost:3000/livros/${livroId}`, { method: 'DELETE' })
+            .then(resposta => {
+
+                let tr = elementoClicado.closest(`#livro_${livroId}`);
+                tr.remove();
+            })
+            .catch(erro => console.log(erro));
+    }
 });
